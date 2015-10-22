c532d4dd29e70d808437eaf4bc4233c74eea7488
TimonYager <ghostship0704@gmail.com>
2015-10-22 12:24:34 +0300

    koan_5 passed

--- knowledge.rb
+++ knowledge.rb
@@ -1,10 +1,21 @@
 class Module
 	def attribute(name)
-		define_method("#{name}=") do |value| 
-			name = value
-		end
-		define_method(name) do
-			name
-		end
+		define_method("#{name}=") { |value|	instance_variable_set("@#{name}", value) }
+		define_method(name) { instance_variable_get("@#{name}") }
+		define_method("#{name}?") { instance_variable_get("@#{name}") ? true :	false } 
 	end
 end
+
+c = Class.new do 
+end
+
+o = Object.new
+
+class << o
+  attribute 'a'
+end
+
+puts not(o.a?)
+puts o.a = 42
+puts o.a == 42
+puts o.a?
